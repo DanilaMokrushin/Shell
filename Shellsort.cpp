11bc3c009fbc74903bddb3e063fa4b0302886643
DanilaMokrushin <dmokr@ya.ru>
2021-04-29 10:27:37 +0300

    Shell

---
+++ Shellsort.cpp
@@ -0,0 +1,46 @@
+﻿// Shellsort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void ShellSort(vector<int>& Vector) {
+	int n = Vector.size();
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+		for (int i = gap; i < n; i ++)
+		{
+			int v = Vector[i];
+			int j = i;
+			while (j >= gap && Vector[j - gap] > v) {
+				Vector[j] = Vector[j - gap];
+				j -= gap;
+			}
+			Vector[j] = v;
+		}
+	}
+}
+
+vector<int> shellsort_mokrushin(vector<int> arr) {
+	ShellSort(arr);
+	return arr;
+}
+int main() {
+vector<int> myArr;
+int index, number, input;
+
+cout << "Enter the number of elements in the array : ";
+cin >> number;
+
+for (int index = 0; index < number; index++) {
+	cin >> input;
+	myArr.push_back(input);
+}
+
+ShellSort(myArr);
+for (int index = 0; index < myArr.size(); index++) {
+	cout << myArr[index] << " ";
+}
+return 0;
+}
